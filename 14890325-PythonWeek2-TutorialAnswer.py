88d6c8584128c7d1f0032071611b0a27ed98a681
Ten Cua Ban <giahiennguyenba@gmail.com>
2026-03-28 18:53:22 +0800

    Added week 4 homework

---
+++ 14890325-PythonWeek2-TutorialAnswer.py
@@ -0,0 +1,13 @@
+# Student ID: 14890325
+# Student Name: Nguyen Ba Gia Hien
+
+my_string = "Taiwan 101"
+
+print(my_string[7:10])  # Output: "101"
+
+print(my_string[3]) 
+
+print(my_string.upper())
+
+print(my_string.replace("101", "101Building"))
+
---
+++ 14890325-PythonWeek3-TutorialAnswer.py
@@ -0,0 +1,15 @@
+#Student ID: 14890325
+#Student Name: Nguyen Ba Gia Hien
+
+age = int(input("Please enter your age: "))
+
+if age <= 12:
+    print("Child")
+elif age >12 and age<= 19:
+    print("Teenager")
+elif age > 19 and age <= 60:            
+    print("Adult")
+elif age > 60:
+    print("Senior")
+
+
